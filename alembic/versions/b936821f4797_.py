74f936a4e4742a0c651d322a2c869570198a8133
Kamila <tashimovakamila1@gmail.com>
2023-07-30 22:36:38 +0600

    migrations

---
+++ alembic/versions/b936821f4797_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b936821f4797
+Revises: 06715cca3725
+Create Date: 2023-07-28 15:31:02.422109
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b936821f4797'
+down_revision = '06715cca3725'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
